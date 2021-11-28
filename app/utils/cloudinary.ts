e32c644c30f41d09b2cdcba80e8d681b5f9c8987
Konrad Erlendsson <konrade@gmail.com>
2021-11-28 17:32:14 +0000

    add error handling to illegal cloudinary urls

--- app/utils/cloudinary.ts
+++ app/utils/cloudinary.ts
@@ -2,16 +2,25 @@ export function applyTransformations(
 	cloudinaryUrl: string,
 	transformations: string[],
 ) {
-	const url = new URL(cloudinaryUrl);
-	const pathSegments = url.pathname.split('/');
-	const transformedPath = pathSegments
-		.map((segment) => {
-			if (segment === 'upload') {
-				return `upload/${transformations.join(',')}`;
-			}
-			return segment;
-		})
-		.join('/');
-	url.pathname = transformedPath;
-	return url.href;
+	try {
+		const url = new URL(cloudinaryUrl);
+		const pathSegments = url.pathname.split('/');
+		const transformedPath = pathSegments
+			.map((segment) => {
+				if (segment === 'upload') {
+					return `upload/${transformations.join(',')}`;
+				}
+				return segment;
+			})
+			.join('/');
+		url.pathname = transformedPath;
+		return url.href;
+	} catch (error) {
+		console.log(
+			'An error occurred when trying to transfrom `%s`:',
+			cloudinaryUrl,
+		);
+		console.error(error);
+		return '';
+	}
 }
